d9b797d447300a487b07d756fb4967f789ae9d4d
Shandy <shandysuprapta@gmail.com>
2023-09-27 18:52:28 +0800

    Update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById("buttonArrow").addEventListener("click", function() {
     const day = document.getElementById("day").value;
     const month = document.getElementById("month").value;
     const year = document.getElementById("year").value;
+    const daysInMonth = new Date(year, month, 0).getDate();
     const currentDate = new Date();
 
     const birthdate = new Date(year, month - 1, day); 
@@ -10,7 +11,18 @@ document.getElementById("buttonArrow").addEventListener("click", function() {
     let months = currentDate.getMonth() - birthdate.getMonth();
     let days = currentDate.getDate() - birthdate.getDate();
 
-    if (birthdate > currentDate || month == 2 && day >= 29) {
+    if (days < 0) {
+        months--; 
+        const lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+        days += lastMonthDate.getDate(); 
+    }
+
+    if (months < 0 || (months === 0 && days < 0)) {
+        years--;
+        months += 12;
+    }
+
+    if ((birthdate > currentDate) || (day > daysInMonth)) {
         document.getElementById("alert-day").textContent = "Must be a valid date";
         document.getElementById("day-label").classList.add("input-group__label--alert");
         document.getElementById("day-label").classList.remove("input-group__label");
@@ -22,15 +34,9 @@ document.getElementById("buttonArrow").addEventListener("click", function() {
         document.getElementById("month").classList.add("input-group__input--alert");
         document.getElementById("year").classList.add("input-group__input--alert");
         return;
+    } else {
+        document.getElementById("years").textContent = years;
+        document.getElementById("months").textContent = months;
+        document.getElementById("days").textContent = days;
     }
-
-
-    if (months < 0 || (months === 0 && days < 0)) {
-        years--;
-        months += 12;
-    }
-
-    document.getElementById("years").textContent = years;
-    document.getElementById("months").textContent = months;
-    document.getElementById("days").textContent = days;
 });
